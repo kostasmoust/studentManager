55f8dfa6c21e9b27acc30aca84a146cc3da2816f
kostasmoust <kostasmoustakaslearning@gmail.com>
2024-04-25 18:34:44 +0300

    Commit 0) Started the file.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.example.studentmanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.studentmanager to javafx.fxml;
+    exports org.example.studentmanager;
+}
